159820af9ddf9aa368f846e8dea8c139b46db9f6
Tony Ramos <tony.ramos.dev@gmail.com>
2024-07-11 21:26:47 -0400

    Initial Commit

---
+++ jram878-Class-Roster/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  jram878-Class-Roster
+//
+//  Created by Juan Ramos on 7/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
